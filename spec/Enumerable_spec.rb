be0ffd7228620e1effc7a13ba1c245c6a46f4d02
Miguel Prada <mapra99@gmail.com>
2019-08-30 11:49:21 -0500

    Added tests for select, coun and map

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -32,11 +32,11 @@ RSpec.describe Enumerable do
         end
         it "checks my_select" do
             my_array = ["hi","hello"].my_select{|x| x == "hi"}
-            array = ["hi","hello"].select{|x| x == "hi"} 
+            array = ["hi","hello"].select{|x| x == "hi"}
             expect(my_array).to eql(array)
             end
     end
-   
+
     describe "my_all?" do
         it "checks my_all?" do
         my_array = [1,2,5,4,6].my_all? {|x|  x > 0}
@@ -49,6 +49,65 @@ RSpec.describe Enumerable do
             expect(my_array).to eql(array)
             end
     end
-    
 
+    describe "my_any?" do
+        it "checks my_any?" do
+        my_array = [1,2,5,4,6].my_any? {|x|  x > 0}
+        array = [1,2,5,4,6].any? {|x| x > 0}
+        expect(my_array).to eql(array)
+        end
+        it "checks my_any?" do
+            my_array = [true,false,true].my_any?
+            array = [true,false,true].any?
+            expect(my_array).to eql(array)
+            end
+    end
+
+    describe "my_none?" do
+        it "checks my_none?" do
+        my_array = [1,2,5,4,6].my_none? {|x|  x > 0}
+        array = [1,2,5,4,6].my_none? {|x| x > 0}
+        expect(my_array).to eql(array)
+        end
+        it "checks my_none?" do
+            my_array = [true,false,true].my_none?
+            array = [true,false,true].my_none?
+            expect(my_array).to eql(array)
+            end
+    end
+
+    describe "my_count" do
+       it "checks my_count" do
+       my_array = [1,2,5,4,6].my_count {|x|  x>2}
+       array = [1,2,5,4,6].count {|x| x>2}
+       expect(my_array).to eql(array)
+       end
+   end
+
+   describe "my_map" do
+     before do
+       @proc = Proc.new {|x| x > 2}
+     end
+
+     describe "my_map_v1" do
+       it "checks my_map_v1 (only blocks)" do
+       my_array = [1,2,5,4,6].my_map_v1 {|x|  x>2}
+       array = [1,2,5,4,6].map {|x| x>2}
+       expect(my_array).to eql(array)
+       end
+     end
+     describe "my_map_v2" do
+       it "checks my_map_v2 (blocks)" do
+       my_array = [1,2,5,4,6].my_map_v2 {|x|  x>2}
+       array = [1,2,5,4,6].map {|x| x>2}
+       expect(my_array).to eql(array)
+       end
+
+       it "checks my_map_v2 (procs)" do
+       my_array = [1,2,5,4,6].my_map_v2(@proc)
+       array = [1,2,5,4,6].map {|x| x>2}
+       expect(my_array).to eql(array)
+       end
+     end
+  end
 end
